52b0b0b1a8b95b04ab1eabfad8fe5ab0f4f4f9fb
Alvo Rithm <alvorithmemail@gmail.com>
2022-08-06 11:40:43 +0200

    rust compiler hello_world progrm with newline characters. Use rusc to compile

---
+++ 01_rust fundamentals/04_hello_world_rust_compiler.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("Hello, world!\n\nHello, Rustaceans!");
+}
+
+// What is println! ? 
+
+// println! is a macro, it’s not a function 
+// Macros end with an exclamation mark:
+// Prints its argument and a newline
+// use rustc compiler rustc to compile rust code
+// use ./executable_compiled_file to run the compiled program
+// \n new line character
+// prints the following:01_introduction_to_rust_fundamentals_hello_world_rust_compiler
+// Hello, world!
+
+// Hello, Rustaceans!
