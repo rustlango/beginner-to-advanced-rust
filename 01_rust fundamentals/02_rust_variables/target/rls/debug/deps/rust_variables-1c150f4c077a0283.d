e88f464f72e850eee6aa5abd3a989db694e52481
Alvo Rithm <alvorithmemail@gmail.com>
2022-08-06 15:08:00 +0200

    .gitignore file updated

--- 01_rust fundamentals/02_rust_variables/target/rls/debug/deps/rust_variables-1c150f4c077a0283.d
+++
@@ -1,6 +0,0 @@
-/home/alvorithm/Documents/diligence_work/fs_bchain/web3-plus-stuff/Rustlanging/moralis_rust_course/m_rust_course/01_rust fundamentals/02_rust_variables/target/rls/debug/deps/rust_variables-1c150f4c077a0283.rmeta: src/main.rs src/integers_mutable_immutable.rs
-
-/home/alvorithm/Documents/diligence_work/fs_bchain/web3-plus-stuff/Rustlanging/moralis_rust_course/m_rust_course/01_rust fundamentals/02_rust_variables/target/rls/debug/deps/rust_variables-1c150f4c077a0283.d: src/main.rs src/integers_mutable_immutable.rs
-
-src/main.rs:
-src/integers_mutable_immutable.rs:
