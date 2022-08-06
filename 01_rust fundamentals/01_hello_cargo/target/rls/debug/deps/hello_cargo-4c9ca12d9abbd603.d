2e063e95ad0a201557ee23657dd4f5574bced5f3
Alvo Rithm <alvorithmemail@gmail.com>
2022-08-06 15:33:43 +0200

    all cargo directories ignore target directoiries - they all contain .gitignore file

--- 01_rust fundamentals/01_hello_cargo/target/rls/debug/deps/hello_cargo-4c9ca12d9abbd603.d
+++
@@ -1,5 +0,0 @@
-/home/alvorithm/Documents/diligence_work/fs_bchain/web3-plus-stuff/Rustlanging/moralis_rust_course/m_rust_course/01_rust fundamentals/hello_cargo/target/rls/debug/deps/hello_cargo-4c9ca12d9abbd603.rmeta: src/main.rs
-
-/home/alvorithm/Documents/diligence_work/fs_bchain/web3-plus-stuff/Rustlanging/moralis_rust_course/m_rust_course/01_rust fundamentals/hello_cargo/target/rls/debug/deps/hello_cargo-4c9ca12d9abbd603.d: src/main.rs
-
-src/main.rs:
