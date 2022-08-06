62f9996f0a8d6b0b3268c3ef8d3beb977419ed63
Alvo Rithm <alvorithmemail@gmail.com>
2022-08-06 11:52:24 +0200

    cargo and npm distinctions made

---
+++ 01_rust fundamentals/06_compare_and_contrast_cargo_with_npm.rs
@@ -0,0 +1,14 @@
+// Research the Internet and learn a bit about both Cargo and the Node 
+// Package Manager (npm).
+
+// 1. What are the main similarities between the two?
+// Cargo and npm are both package managers and they share similar commands 
+// in a way.
+// 2. Are there any significant differences?
+// Cargo is used for building, running and managing rust programs/projects. 
+// Npm is used for building, running and managing node.js related projects. 
+// npm is used to install packages. Cargo is used to install crates.
+// 3. What is the equivalent of cargo new in the npm world?
+// npm init is similar to cargo new for initialising new projects.
+// 4. What is the equivalent of package.json in the cargo world?
+// Cargo uses a cargo.toml file to manage dependencies and developer dependencies.
