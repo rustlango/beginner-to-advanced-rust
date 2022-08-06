b26f0ace796f9b8513e2ff6b7176275012f620f5
Alvo Rithm <alvorithmemail@gmail.com>
2022-08-06 14:25:47 +0200

    brought all modules using the shortcut: use integers_mutable_immutable::*; works when there are a lot of modules in a library file

--- 01_rust fundamentals/02_rust_variables/src/main.rs
+++ 01_rust fundamentals/02_rust_variables/src/main.rs
@@ -1,5 +1,6 @@
 mod integers_mutable_immutable;
-use integers_mutable_immutable::{add, unsigned_integers, uint_min_max, signed_integers_min_max};
+// use integers_mutable_immutable::{add, unsigned_integers, uint_min_max, signed_integers_min_max};
+use integers_mutable_immutable::*;
 
 fn main() {
     add::adding();
